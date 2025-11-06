3cfc3bdc70b7005de9be4c020770164b9ba7b05d
daey0ung <radio010719@gmail.com>
2025-11-06 17:05:48 +0900

    Initial commit

---
+++ py/opencv/00_opencv 다운로드 및 버전 확인.py
@@ -0,0 +1,17 @@
+# opencv 설치 방법
+# 방법 1.  pip install opencv-python
+# 방법 1-1. 최신 버전으로 설치된다.
+# 방법 2. pip install opencv-python== 4.12.0.88
+# 방법 2-1. 원하는 버전을 지정해서 다운받는다 / 버전 확인 사이트 : https://pypi.org/project/opencv-python/#history
+# 방법 3. import cv2에서 빨간줄이 뜨거나 실행 시 패키지 설치라고 뜨는 버튼 클릭 후 다운로드
+
+# opencv 삭제 방법
+# 방법 1. pip uninstall opencv-python
+
+# opencv 설치 유뮤 확인 방법
+# 방법 1. 아래 입력 후 실행
+import cv2
+print(cv2.__version__)
+
+# 방법 2. 터미널에 pip show opencv-python 입력 후 엔터
+
